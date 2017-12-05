1c0a21bb6ad3b442115fff2e499d5cc0de96c2a3
roadfun <ronaldg@PowerHouse.local>
2017-12-05 11:27:43 -0600

    Initial Github import

---
+++ src/GarageMonitor.cpp
@@ -0,0 +1,187 @@
+
+// Project - Feather Huzzah version
+//  Monitor the garage door with a sensor. If it is open more than openMinutes send a push notification
+//
+//  Notification is sent by using the Twilio REST API service.
+//
+
+#include "GarageMonitor.h"
+
+int doorState(int);
+void aio_Status(char*);
+void aio_Heartbeat();
+void sendAlert(int);
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(inputPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);          // Start with LED off
+
+  // Connect to WiFi access point.
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("WiFi connected: "); Serial.println(WiFi.localIP());
+
+  twilio = new Twilio(account_sid, auth_token, fingerprint);
+
+  // pause for serial connection for debugging, don't block in case in production mode
+  // On the Huzzah, when this was right after Serial.begin the Wifi connection never worked
+  delay(5000);
+
+  // Get the baseline time and door state
+  stateChangeTime = now();
+  currentState = doorState(inputPin);
+  previousState = currentState;
+
+  aio_Status("Guardian Started");
+  sendAlert(READY);
+}
+
+void loop() {
+  currentState = doorState(inputPin);
+  // If the state changed, & door had been opened > openMinutes send a SMS and update AIO
+  // Set the previousState and note when the time when the state changed
+  if (currentState != previousState) {
+    if ((previousState == doorOpen)  && ((now() - stateChangeTime) > (openMinutes * 60))) {
+      // State went from Open to Closed
+      alarmSent = false;
+      sendAlert(G_CLOSED);
+      aio_Status("Garage closed");
+    }
+    previousState = currentState;
+    stateChangeTime = now();
+  }
+
+  // If the state is open, see if it has been open long enough to sound the alarm
+  if (currentState == doorOpen) {
+    if ((now() - stateChangeTime) > (openMinutes * 60) && !alarmSent) {
+      sendAlert(OPEN);
+      aio_Status("Garage is open!");
+      alarmSent = true;
+    }
+  }
+
+  if ((minute() == 0) && (second() == 0)) {
+    // Send a heartbeat message to AIO once an hour
+    // Note since this has no real clock, this check is not actually at 00:00 in real terms
+    aio_Heartbeat();
+  }
+  delay(pollTime);    // Throttle loop speed, should be at least 1 second
+}
+
+// Returns the current state of the door, debounced for debounceDelay ms
+// If we don't do this and get bouncy state changes we would end up resetting the countdown of stateMinutes erroneously
+int doorState (int pin) {
+  boolean newState;
+  boolean initialState;
+  int debounceDelay = 10;    // ms
+
+  initialState = digitalRead(pin);
+  // Debounce logic - state must stay the same for debounceDelay ms or considered noise
+  for (int count; count < debounceDelay; count++) {
+    delay(1);
+    newState = digitalRead(pin);
+    // If the state changed then start over
+    if (newState != initialState) {
+      count = 0;
+      initialState = newState;
+    }
+  }
+  //Serial.print("Door State "); Serial.println(initialState);
+  if (initialState == 0) {
+    // Door is closed
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+  return initialState;
+}
+
+void sendAlert(int alertType) {
+  switch (alertType) {
+    case READY:
+                messageBody = "Guardian Ready";
+                break;
+    case OPEN:
+                messageBody = "Garage is Open!";
+                break;
+    case G_CLOSED:
+                messageBody = "Garage has Closed";
+                break;
+    default:
+//                Serial.println("Invalid alertType");
+                break;
+  }
+  Serial.println(messageBody);
+
+  // Response will be filled with connection info and Twilio API responses
+  String response;
+  bool success = twilio->send_message(
+        to_number, from_number,
+        messageBody,
+        response,
+        media_url
+  );
+
+  if (success) {
+    Serial.println("Success sending SMS");
+  } else {
+    Serial.println("POST to twilio failed: ");
+    Serial.println(response);
+  }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
+
+void aio_Status(char *message) {
+  MQTT_connect();
+
+  if (! statusFeed.publish(message)) {
+    Serial.println(F("MQTT send Failed"));
+  } else {
+    Serial.println(F("MQTT send OK!"));
+  }
+}
+
+void aio_Heartbeat() {
+  MQTT_connect();
+
+  if (! heartBeatFeed.publish("I'm alive!")) {
+    Serial.println(F("MQTT send Failed"));
+  } else {
+    Serial.println(F("MQTT send OK!"));
+  }
+}
---
+++ src/GarageMonitor.h
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <AdafruitIO.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include "twilio.hpp"
+#include <TimeLib.h>
+#include "credentials.h"
+
+boolean currentState, previousState;
+time_t stateChangeTime;
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+void MQTT_connect();
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+// Setup a feed called 'Guardian Status' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish statusFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Guardian Status");
+Adafruit_MQTT_Publish heartBeatFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Guardian Heartbeat");
+
+// Setup a feed called 'onoff' for subscribing to changes.
+//Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
+
+Twilio *twilio;
+
+// Optional - a url to an image.  See 'MediaUrl' here:
+// https://www.twilio.com/docs/api/rest/sending-messages
+String media_url = "";
+
+// Pin assignments
+const int ledPin = 2;                 // Blue LED
+const int inputPin = 5;               // Digital input pin for the proximity sensor
+                                       // Pin 4 on Huzzah wouldn't work (lacks pullup?)
+                                       // Pin 2 does not allow sketch uploading
+                                       // Tying inputPin to ground yields Door state 0
+                                       // Disconnecting inputPin and letting it float yelds Door state 0
+                                       // Connecting inputPin to 3V yields Door state 1
+
+const String garageClosed = "closed";
+const String garageOpen = "open";
+
+boolean alarmSent = false;            // True if we already sent the Open alarm, don't want to keep sending it
+
+// "Production" values
+//const unsigned long oneMinute = 60L*1000L;   // One minute is how often we check to see if door still open/closed
+const unsigned long openMinutes = 5;         // Door must be open this many minutes before alert sent
+
+// Debug values
+//const unsigned long oneMinute = 15L*1000L;
+//const int openMinutes = 2;
+//const int closedMinutes = 1;
+//const int waitingMinutes = 1;
+
+const int pollTime = 1000;            // How long to sleep between checks for door open/closed: 1000 = 1 second (lots of debug output)
+//const int pollTime = 250;
+//const int pollTime = 15000;            // How long to sleep between checks for door open/closed: 15000 == 15 seconds
+
+//  These make the code more descriptive and can be swapped for NO/NC sensors
+#define doorOpen HIGH
+#define doorClosed LOW
+
+// Values for the type of alert to send
+const int READY=1;
+//const int RUNNING=2;
+const int OPEN=3;
+const int G_CLOSED=4;
+
+void sendAlert();
+int doorState();
